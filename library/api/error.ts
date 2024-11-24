dd0eab3a37531140f1eaba4df7d5ba418c27a73b
tamaco489 <tamaco489@gmail.com>
2024-11-24 16:47:11 +0900

    fix comment

--- library/api/error.ts
+++ library/api/error.ts
@@ -5,11 +5,21 @@
  * API レスポンスがエラーを返した場合に、このクラスをインスタンス化してエラーハンドリングを行います。
  */
 export default class HTTPError extends Error {
+  /**
+   * エラーの HTTP ステータスコードを保持します。
+   * 例: 404 (Not Found), 500 (Internal Server Error) など。
+   */
   status: number;
 
+  /**
+   * HTTPError クラスのインスタンスを生成します。
+   *
+   * @param {number} status - HTTP ステータスコード。
+   * @param {string} message - エラーの詳細メッセージ。
+   */
   constructor(status: number, message: string) {
-    super(message);
-    this.status = status;
-    this.name = 'HTTPError';
+    super(message); // 親クラス (Error) のコンストラクタを呼び出し、メッセージを設定します。
+    this.status = status; // エラーの HTTP ステータスコードを設定します。
+    this.name = 'HTTPError'; // エラー名を "HTTPError" に設定します (Error.name のオーバーライド)。
   }
 }
